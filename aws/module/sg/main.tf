719966d6c7644e5b61bdb771b73cf09a7643022f
fn <francis.nwoguh@gmail.com>
2023-07-24 22:02:08 -0700

    created child modules for various resources

---
+++ aws/module/sg/main.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "ssh_sg" {
+  description = "allow ssh traffic"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "ssh"
+    from_port = var.ssh_port
+    to_port = var.ssh_port
+    protocol = var.protocol
+    cidr_block = var.cidr_block
+  }
+
+  egress {
+    description = "ssh"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-sg"
+  }
+}
+
+resource "aws_security_group" "http_sg" {
+  description = "allow http traffic"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "http"
+    from_port = var.http_port
+    to_port = var.http_port
+    protocol = var.protocol
+    cidr_block = var.cidr_block
+  }
+
+  egress {
+    description = "http"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "http-sg"
+  }
+}
+
--- aws/module/vpc/variable.tf
+++ aws/module/vpc/variable.tf
@@ -1,1 +1,43 @@
-variable "name" {}
+variable "env" {}
+variable "azs" {}
+variable "" {}
+variable "enable_dns_hostnames" {
+  description = "should be true to enable DNS hostname in vpc"
+  default = bool
+}
+
+variable "enable_dns_support" {
+  description = "should be true to enable DNS support in vpc"
+  default = bool
+}
+
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+variable "instance_tenancy" {
+  type = string
+  default = "default"
+}
+
+#public subnet
+variable "public_subnets" {}
+variable "map_public_ip_on_launch" {
+  type = bool
+  default = true
+}
+
+#private subnet
+variable "private_subnet" {}
+variable "enable_nat_gateway" {
+  type = bool
+  default = true
+}
+variable "single_nat_gateway" {
+  type = bool
+  default = true
+}
+
+variable "destination_cidr_block" {
+  default = "0.0.0.0/0"
+}

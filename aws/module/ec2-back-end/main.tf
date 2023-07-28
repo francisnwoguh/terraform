84380fe10afd66d4c1440f14ff8255d796588c06
fn <francis.nwoguh@gmail.com>
2023-07-28 15:15:57 -0700

    Added terraform configuration module for ec2 server

---
+++ aws/module/ec2-back-end/main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "front-end" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  key_name = var.key_name
+  }

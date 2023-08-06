8df2d84fb2345872aa7777ae61a93a1c175577ff
fn <francis.nwoguh@gmail.com>
2023-08-06 01:15:56 -0700

    updated configs

--- aws/project-vpc/main.tf
+++ aws/project-vpc/main.tf
@@ -14,11 +14,24 @@ terraform {
 locals {
   env    = "dev"
   region = "us-east-1"
+  name   = "bastion-host"
+  project = "project-vpc"
 }
+
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
 
 module "project-vpc" {
   source               = "../../aws/module/vpc"
-  name                 = "${local.env}-${var.name}"
+  name                 = "${local.env}-${local.project}"
   azs                  = var.azs
   private_subnets      = var.private_subnets
   public_subnets       = var.public_subnets
@@ -30,4 +43,14 @@ module "vpc_sg" {
   source = "../../aws/module/sg"
   name   = "${local.env}-sg"
   vpc_id = module.project-vpc.vpc_id
+}
+
+module "bastion-host" {
+  source                      = "../../aws/module/bastion"
+  name                        = local.name
+  instance_type               = var.instance_type
+  ami                         = data.aws_ami.amzn-linux-2023-ami.id
+  vpc_security_group_ids      = [module.vpc_sg.ssh_sg, module.vpc_sg.http_sg]
+  subnet_id                   = element(module.project-vpc.public_subnets, 0)
+  associate_public_ip_address = var.associate_public_ip_address
 }
